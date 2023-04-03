79c34f5104c24c54b33de2fe94f0bb99aad4883d
Mikael Simberg <mikael.simberg@iki.fi>
2023-04-03 11:11:34 +0200

    Add some more test cases for bulk_nested
    Allow deeper levels, but ignore them in default implementation.

--- test/exec/test_bulk_nested.cpp
+++ test/exec/test_bulk_nested.cpp
@@ -23,20 +23,45 @@
 #include <stdexec/execution.hpp>
 
 TEST_CASE("bulk_nested compiles", "[adaptors][bulk_nested]") {
-  stdexec::sender auto snd = exec::bulk_nested(
-      stdexec::just(42), std::array{10},
-      [](stdexec::scheduler auto sch, int i, int &x) {
-        std::cerr << "hello from outer index " << i
-                  << " with inline scheduler\n";
-        stdexec::sync_wait(
-            stdexec::schedule(sch) |
-            exec::bulk_nested(
-                std::array{3}, [](stdexec::scheduler auto, int j) {
-                  std::cerr << "hello from inner index " << j
-                            << " with subscheduler of inline scheduler\n";
-                }));
-      });
-  stdexec::sync_wait(std::move(snd));
+  {
+    stdexec::sender auto snd = exec::bulk_nested(
+        stdexec::just(42), std::array{10},
+        [](stdexec::scheduler auto sch, int i, int &x) {
+          std::cerr << "hello from outer index " << i
+                    << " with inline scheduler\n";
+          stdexec::sync_wait(
+              stdexec::schedule(sch) |
+              exec::bulk_nested(
+                  std::array{3}, [](stdexec::scheduler auto, int j) {
+                    std::cerr << "hello from inner index " << j
+                              << " with subscheduler of inline scheduler\n";
+                  }));
+        });
+    stdexec::sync_wait(std::move(snd));
+  }
+
+  {
+    stdexec::sender auto snd = exec::bulk_nested(
+        // Levels 2 onwards are completely ignored in the default case. They
+        // don't actually require any setup in the default case. Should they be
+        // allowed?
+        stdexec::just(42), std::array{10, 17, 7},
+        [](stdexec::scheduler auto sch, int i, int &x) {
+          std::cerr << "hello from outer index " << i
+                    << " with inline scheduler\n";
+          stdexec::sync_wait(
+              stdexec::schedule(sch) |
+              // Can specify any hierarchy here again. The second level from
+              // above is completely decoupled (ignored) from the level
+              // specified above. That's ok!?
+              exec::bulk_nested(
+                  std::array{3}, [](stdexec::scheduler auto, int j) {
+                    std::cerr << "hello from inner index " << j
+                              << " with subscheduler of inline scheduler\n";
+                  }));
+        });
+    stdexec::sync_wait(std::move(snd));
+  }
 }
 
 TEST_CASE("bulk_nested compiles with thread pool scheduler",
@@ -45,8 +70,10 @@ TEST_CASE("bulk_nested compiles with thread pool scheduler",
 
   stdexec::sender auto snd =
       stdexec::transfer_just(pool_.get_scheduler(), 42) |
-      exec::bulk_nested(std::array{10}, [](stdexec::scheduler auto sch, int i,
-                                           int &x) {
+      // Levels 2- are completely ignored. They don't actually require any
+      // setup in the default case. Should they be allowed?
+      exec::bulk_nested(std::array{10, 17, 7}, [](stdexec::scheduler auto sch,
+                                                  int i, int &x) {
         std::cerr << "hello from outer index " << i
                   << " with static thread pool scheduler\n";
         stdexec::sync_wait(
